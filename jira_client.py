c0807049a9cda667fec2e804edea696cf130ad84
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2024-02-08 13:38:24 +0100

    Remove whitespaces

--- jira_client.py
+++ jira_client.py
@@ -4,13 +4,12 @@ from requests.auth import HTTPBasicAuth
 SUCCESS = 200
 
 class JiraClient:
-    
-    
+
     def __init__(self, email, api_token, domain):
         self.auth = HTTPBasicAuth(email, api_token)
         self.headers = { "Accept": "application/json", "Content-Type": "application/json" }
         self.base_url = f'https://{domain}'
-        
+
     def searchIssues(self, jql_query, max_results = 50, fields = 'key,summary,status'):
         api_endpoint = '/rest/api/3/search'
         query = { 'jql': jql_query, 'maxResults': max_results, 'fields': fields }
@@ -19,7 +18,7 @@ class JiraClient:
             return response.json()['issues']
         else:
             print('Failed to search issues:', response.status_code, response.text)         
-            
+
     def sortByStatus(self, issues):
         issues_by_status = {}
         for issue in issues:
